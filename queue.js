20bedf777ffe05ca3901f76b917c53b78a8eb58b
Samux33 <samuel.tarifa.dev@gmail.com>
2024-06-07 16:13:11 -0300

    starting on custom data structures

---
+++ queue.js
@@ -0,0 +1,20 @@
+class Queue{
+  constructor(){
+    this.items=[]
+  }
+  enqueue(item){
+    this.items.push(item)
+  }
+  dequeue(){
+    this.items.unshift()
+  }
+  peek(){
+    return this.items[0]
+  }
+  isEmpty(){
+    return this.items.length===0
+  }
+  size(){
+    return this.items.length
+  }
+}
---
+++ substring.js
@@ -0,0 +1,10 @@
+// Dada una cadena S, escribe una función que encuentre la subcadena (substring) más larga que aparezca al menos dos veces en S. La subcadena debe ser continua y no debe solaparse. La función debe devolver la subcadena más larga. Si hay múltiples subcadenas con la misma longitud máxima, devuelve cualquiera de ellas. Si no hay ninguna subcadena que se repita, devuelve una cadena vacía.
+function longestSubstring(s){
+  let result=''
+  for (let i=0;i<s.length;i++){
+    
+  }
+  return result
+}
+
+console.log(longestSubstring(`abcab`)); // ab
