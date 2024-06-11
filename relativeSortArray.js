ac736dd54684979a702536036d5f9e2a212ea038
Samux33 <samuel.tarifa.dev@gmail.com>
2024-06-11 20:55:08 -0300

    another solution to relativeSortArray

--- relativeSortArray.js
+++ relativeSortArray.js
@@ -7,7 +7,7 @@ Sort the elements of arr1 such that the relative ordering of items in arr1 are t
 should be placed at the end of arr1 in ascending order.
 */
 
-var relativeSortArray = function (arr1, arr2) {
+var relativeSortArray1 = function (arr1, arr2) {
   const result = [];
 
   for (let i = 0; i < arr2.length; i++) {
@@ -15,18 +15,37 @@ var relativeSortArray = function (arr1, arr2) {
       if (arr2[i] === arr1[j]) {
         result.push(arr1[j]);
         arr1.splice(j, 1);
-        j--
+        j--;
       }
     }
   }
 
-  const sorted=arr1.sort((a,b)=>a-b)
+  const sorted = arr1.sort((a, b) => a - b);
 
   return result.concat(sorted);
+};
 
+var relativeSortArray2 = function (arr1, arr2) {
+  const sortedList = [];
+
+  for (const n of arr2) {
+    let index = -1;
+    index = arr1.indexOf(n, index + 1);
+
+    while (index !== -1) {
+      sortedList.push(n);
+      arr1[index] = null;
+      index = arr1.indexOf(n, index + 1);
+    }
+  }
+
+  return [
+    ...sortedList,
+    ...arr1.filter((a) => a !== null).sort((a, b) => a - b),
+  ];
 };
 
 const arr1 = [2, 3, 1, 3, 2, 4, 6, 19, 9, 2, 7];
 const arr2 = [2, 1, 4, 3, 9, 6];
 
-console.log(relativeSortArray(arr1, arr2));
+console.log(relativeSortArray2(arr1, arr2));
