9a3c4e3cfae2e99e4ddb40a394ef91df6d29ba86
Samux33 <samuel.tarifa.dev@gmail.com>
2024-06-11 20:39:21 -0300

    relative sort resuelto

--- relativeSortArray.js
+++ relativeSortArray.js
@@ -1,19 +1,23 @@
 var relativeSortArray = function (arr1, arr2) {
-  const result = []
-
-  const n=arr1.length
+  const result = [];
 
   for (let i = 0; i < arr2.length; i++) {
-      for (let j = 0; j < n; j++) {
-          if (arr2[i] === arr1[j]) {
-              result.push(arr1[j])
-              arr1.splice(j, 1)
-          }
+    for (let j = 0; j < arr1.length; j++) {
+      if (arr2[i] === arr1[j]) {
+        result.push(arr1[j]);
+        arr1.splice(j, 1);
+        j--
       }
+    }
   }
 
-  result.concat(arr1.sort((a, b) => a - b))
+  const sorted=arr1.sort((a,b)=>a-b)
 
-  return result
+  return result.concat(sorted);
 
 };
+
+const arr1 = [2, 3, 1, 3, 2, 4, 6, 19, 9, 2, 7];
+const arr2 = [2, 1, 4, 3, 9, 6];
+
+console.log(relativeSortArray(arr1, arr2));
