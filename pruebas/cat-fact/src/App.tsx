dd453491e4946d7f822f10c800aa0398a1e5f837
Samux33 <samuel.tarifa.dev@gmail.com>
2024-06-15 12:03:40 -0300

    optimizations

--- pruebas/cat-fact/src/App.tsx
+++ pruebas/cat-fact/src/App.tsx
@@ -6,12 +6,17 @@ function App() {
   const { catFact: fact } = useCatFact();
   const { catImgUrl } = useCatImg(fact);
 
+  console.log('render')
   return (
     <main className="flex flex-col items-center my-8 w-1/2 p-4">
       <h1 className="text-4xl font-bold">Cat Fact Prueba técnica React</h1>
       {!fact && <h3 className="text-pretty">Cargando fact...</h3>}
       {fact && <h3 className="text-pretty">Fact: {fact}</h3>}
-      {catImgUrl && <img className="w-full" src={catImgUrl} alt={fact} />}
+      {catImgUrl ? (
+        <img className="w-full" src={catImgUrl} alt={fact} />
+      ) : (
+        <h2>Cargando imagen...</h2>
+      )}
     </main>
   );
 }
--- pruebas/cat-fact/src/hooks/useCatImg.ts
+++ pruebas/cat-fact/src/hooks/useCatImg.ts
@@ -5,12 +5,16 @@ export default function useCatImg(fact: string) {
   const CAT_IMAGE_PREFIX = "https://cataas.com";
   const NUMBER_WORDS = 3;
   const [catImgId, setCatImgId] = useState();
-  const words = fact.split(" ").slice(0, NUMBER_WORDS).join(" ");
+  const words = fact ? fact.split(" ").slice(0, NUMBER_WORDS).join(" ") : "";
 
   useEffect(() => {
     if (!fact) return;
     getCatImg(words).then((data) => setCatImgId(data["_id"]));
   }, [words, fact]);
 
-  return { catImgUrl: `${CAT_IMAGE_PREFIX}/cat/${catImgId}/says/${words}` };
+  const catImgUrl = catImgId
+    ? `${CAT_IMAGE_PREFIX}/cat/${catImgId}/says/${words}`
+    : null;
+
+  return { catImgUrl };
 }
